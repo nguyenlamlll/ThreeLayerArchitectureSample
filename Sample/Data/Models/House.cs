d2635e59c5cd485c46a8ade430b7e87fa8d04e15
kynguyen2408 <38316580+kynguyen2408@users.noreply.github.com>
2018-04-16 13:01:48 +0700

    Initialize the projects

---
+++ Sample/Data/Models/House.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class House
+    {
+        // Field
+        string number;
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                if (value.GetType() != typeof(int))
+                {
+                    return;
+                }
+                number = value;
+            }
+        }
+
+        // Property
+        public int NumberOfFloors { get; set; }
+        public int NumberOfRooms { get; set; }
+        public string Street { get; set; }
+        public string Ward { get; set; }
+        public string Dicstrict { get; set; }
+
+    }
+    internal class TestHouse // Used inside this assembly only
+    {
+        public House House { get; set; }
+
+        private void TestMethod()
+        {
+            House.NumberOfFloors = 5;
+        }
+    }
+}
---
+++ Sample/Presentation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputList = new List<string>();
+            for (int i = 0; i < 3; i++)
+            {
+                string input = Console.ReadLine();
+                inputList.Add(input);
+            }
+            foreach (var item in inputList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
